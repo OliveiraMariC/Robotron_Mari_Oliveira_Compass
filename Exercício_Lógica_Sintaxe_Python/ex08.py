f0ef83c052241c4acc3bc4570a5a1d1e68e73962
Mari de Oliveira <109982238+OliveiraMariC@users.noreply.github.com>
2022-08-04 04:11:48 -0300

    Update ex08.py
    Alteração no código

---
+++ Exercício_Lógica_Sintaxe_Python/ex08.py
@@ -0,0 +1,32 @@
+def linha():
+    print('-=-' * 21)
+
+
+def fat(num):
+    if num >= 0:
+        fat = num
+        for i in range(num, 0, -1):
+            fat = fat * i
+        linha()
+        print('\nO número', num, 'é par e o resultado de sua fatoração é igual a', fat, '.')
+    else:
+        linha()
+        print('\nO número', num, ' é par mas não existe fatorial para número negativo.')
+
+
+def tabuada(n):
+    print('\nSeu número é ímpar vou te apresentar a tabuada deste número é:\n')
+    linha()
+    for i in range(0, 11):
+        mult = i * n
+        print(n, 'x', i, '=', mult)
+    linha()
+
+
+linha()
+num = int(input('\nInforme um número inteiro: '))
+if num % 2 == 0:
+    print(fat(num))
+    linha()
+else:
+    tabuada(num)
