3e04f076bf0fd02a4dad2d011550fa7d2fa773c7
OliveiraMariC <maricristinadeoliveira@yahoo.com.br>
2022-08-05 16:47:14 -0300

    fim

--- Exercícios_Manipulação_Arquivos_Python/ex16b.py
+++ Exercícios_Manipulação_Arquivos_Python/ex16b.py
@@ -2,12 +2,21 @@
 
 import pandas as pd
 
-tabPeriodica = pd.read_csv('Exercícios_Manipulação_Arquivos_Python\TabPeriodica.csv', sep=",")
-
+tabPeriodica = pd.read_csv('Robotron_Mari_Oliveira_Compass\Exercícios_Manipulação_Arquivos_Python\TabPeriodica.csv', sep=",")
+def menu():
+    print('=-=' * 22)
+    print('TABELA PERIóDICA')
+    print('=-=' * 22)
+    print('1) Símbolo       4) Linha')
+    print('2) Nome          5) Coluna')
+    print('3) Nº Atômico    6) Estado')
+    
+menu()
 print('Escolha um dos símbolos abaixo para buscar os dados desse elemento:')
-tabPeriodica = tabPeriodica['Símbolo']
+
+tabPeriodica = tabPeriodica
 print(tabPeriodica)
 
 simb=input("Digite o símbolo escolhido: ")
-print(" ")
-print(tabPeriodica[tabPeriodica["Simbolo"] == (simb)])
+
+print(tabPeriodica,simb,'\n')
